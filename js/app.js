377f850a749fcbb68298c94016bba2e72a5c3eaf
Steve Cherry <stevenccherry@gmail.com>
2016-07-27 18:19:21 -0400

    Add template include function

--- js/app.js
+++ js/app.js
@@ -5,10 +5,13 @@
 (function ($) {
   var $view = $('#view');
 
+  // Request and render a template
   function render(view) {
     var template = window.location.hash.replace(/[#\/]+/,'') || 'index';
 
-    $.ajax('templates/' + template + '.html').done(function(data) {
+    $.ajax({
+      url: 'templates/' + template + '.html'
+    }).done(function (data) {
       view.html(data);
 
       // Signal template loaded
@@ -16,10 +19,31 @@
     });
   }
 
+  // Include additional templates
+  function include() {
+    var $include = $('[data-include]');
+
+    $include.each(function () {
+      var path = $(this).attr('data-include');
+
+      $.ajax({
+        context: this,
+        url: path
+      }).done(function (data) {
+        $(this).html(data);
+      });
+    });
+  }
+
   // Initial page render
   render($view);
 
+  // Rerender on page change
   $(window).on('hashchange', function() {
     render($view);
+  });
+
+  $(document).on('XHRContentLoaded', function () {
+    include();
   });
 })(jQuery);
